9dd4297bb544a5b7d8e5cbbefd06577a77461efd
Madhumanti17 <madhumantighosh17@gmail.com>
2021-05-07 17:27:47 +0530

    Commit again

--- First-R-Projecttext.R
+++ First-R-Projecttext.R
@@ -1,3 +1,7 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+git config --global user.email "madhumantighosh17@gmail.com"
+git config --global user.name "Madhumanti Ghosh"
+
+print("This file is now updated within RStudio")
